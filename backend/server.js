d3e7b08195f0fb83b29ed61c14c6875597852f8d
Adi Jain <114348511+Adij003@users.noreply.github.com>
2025-02-02 15:45:04 +0530

    production ready

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 
@@ -13,12 +14,19 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'welcome to the support desk API'})
-})
+
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/faqs', require('./routes/faqRoutes'))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({message: 'welcome to the FAQ App'})
+    })
+}
 
 app.use(errorHandler)
 
